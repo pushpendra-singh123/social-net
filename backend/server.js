4cef152dcf86796803205827a4bcbbeaf34e1f6f
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-01-20 13:55:29 +0530

    project-setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDb.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/notifications", notificationRoutes);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
