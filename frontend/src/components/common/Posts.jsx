db27e2fccbf7511d9df292d517c7ee88d04fdfb7
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-04-04 18:17:53 +0530

    work on posts

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,23 +1,23 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const Posts = () => {
-  const isLoading = false;
+//  write code here, Neha.
 
   return (
     <>
-      {isLoading && (
+      {(isLoading || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
-        <p className="text-center my-4">No posts in this tab. Switch 👻</p>
+      {!isLoading && !isRefetching && POSTS?.length === 0 && (
+        <p className="text-center my-4">No posts in this tab. Switch</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && !isRefetching &&  POSTS && (
         <div>
           {POSTS.map((post) => (
             <Post key={post._id} post={post} />
