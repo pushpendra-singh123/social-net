f59f4cbe093303246029dcd44dbdf4e9fe615244
Muskanvarshney2323 <varshneymuskan077@gmail.com>
2025-04-10 15:02:21 +0530

    updated create post

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,23 +2,53 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
 const CreatePost = () => {
-  const [text, setText] = useState("");
-  const [img, setImg] = useState(null);
+	const [text, setText] = useState("");
+	const [img, setImg] = useState(null);
+	const imgRef = useRef(null);
 
-  const imgRef = useRef(null);
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+	const queryClient = useQueryClient();
 
-  const isPending = false;
-  const isError = false;
+	const {
+		mutate: createPost,
+		isPending,
+		isError,
+		error,
+	} = useMutation({
+		mutationFn: async ({ text, img }) => {
+			try {
+				const res = await fetch("/api/posts/create", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text, img }),
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
 
-  const data = {
-    profileImg: "/avatars/boy1.png",
-  };
+		onSuccess: () => {
+			setText("");
+			setImg(null);
+			toast.success("Post created successfully");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+	});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("Post created successfully");
+    createPost({ text, img });
   };
 
   const handleImgChange = (e) => {
