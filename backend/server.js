9384c7123e18646de2e26389fe9ccd2319c63454
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-04-24 05:46:08 +0530

    updating file names

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
-import notificationRoutes from "./routes/notification.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
